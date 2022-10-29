339897fd8724b71656db690b865db308b0401662
Iqbal Nur Haq <iqbalnur833@gmail.com>
2022-10-29 12:55:49 +0700

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Iqbal Nur Haq on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
